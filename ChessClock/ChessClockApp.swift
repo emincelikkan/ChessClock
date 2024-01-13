fac296303d6999616ed4834efe46c6a46723078a
emincelikkanmobven <emin.celikkan@mobven.com>
2024-01-13 19:05:46 +0300

    Initial Commit

---
+++ ChessClock/ChessClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessClockApp.swift
+//  ChessClock
+//
+//  Created by Emin Çelikkan on 13.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChessClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
